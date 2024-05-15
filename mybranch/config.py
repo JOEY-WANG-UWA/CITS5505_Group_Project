95b257760526f8f04dc0fdffcea4f282511b2bea
LizzzzHFFF <lingzihuangfu@gmail.com>
2024-05-15 14:59:55 +0800

    add application's various settings to test

---
+++ mybranch/config.py
@@ -0,0 +1,19 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    POSTS_PER_PAGE = 3
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    ADMINS = ['joey.wang.uwa@gmail.com']
+    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
+    MAX_CONTENT_LENGTH = 20 * 1024 * 1024  ###
+    DROPZONE_UPLOAD_MULTIPLE = True
+    DROPZONE_ALLOWED_FILE_CUSTOM = True
+    DROPZONE_MAX_FILE_SIZE = 20
+    DROPZONE_PARALLEL_UPLOADS = 9
+    UPLOAD_FOLDER = os.path.join(basedir, 'app', 'static', 'img')
+    AVATAR_UPLOAD_FOLDER = os.path.join(basedir, 'app', 'static', 'img', 'avatar')
