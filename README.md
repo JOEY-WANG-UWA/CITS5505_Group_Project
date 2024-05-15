61111be96368c7d0adc18641a60b2b7b8e96c6b8
LizzzzHFFF <lingzihuangfu@gmail.com>
2024-05-15 15:37:03 +0800

    Clear out all contents

--- README.md
+++
@@ -1,2 +0,0 @@
-# CITS5505_Group_Project
-This is the repository for CITS5505 Group Project
--- microblog/app/microblog.py
+++
@@ -1,7 +0,0 @@
-from app import app
-from app.models import User, Post, Message, Notification
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post,
-            'Message': Message, 'Notification': Notification}
