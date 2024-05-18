08c18d2216edb21f2c254707dba60de7bf0b34c6
LizzzzHFFF <lingzihuangfu@gmail.com>
2024-05-18 15:33:39 +0800

    recover clooection and comment function"

--- microblog/app/main_routes.py
+++ microblog/app/main_routes.py
@@ -68,25 +68,29 @@ def gallery():
 
 
 @main_bp.route('/add_to_collection/<int:upload_id>', methods=['POST'])
-@login_required
 def add_to_collection(upload_id):
-    if not current_user.is_authenticated:
-        return jsonify({'success': False, 'message': 'Authentication required'}), 401
+    try:
 
-    collection = Collection.query.filter_by(
-        upload_id=upload_id, user_id=current_user.id).first()
-    if collection:
-        db.session.delete(collection)
-        db.session.commit()
-        new_count = Collection.query.filter_by(upload_id=upload_id).count()
-        return jsonify({'success': True, 'newCount': new_count, 'liked': False})
-    else:
-        new_collection = Collection(
-            upload_id=upload_id, user_id=current_user.id)
-        db.session.add(new_collection)
-        db.session.commit()
-        new_count = Collection.query.filter_by(upload_id=upload_id).count()
-        return jsonify({'success': True, 'newCount': new_count, 'liked': True})
+        print(
+            f"Received request to add to collection: upload_id={upload_id}, user_id={current_user.id}")
+
+        collection = Collection.query.filter_by(
+            upload_id=upload_id, user_id=current_user.id).first()
+        if collection:
+            db.session.delete(collection)
+            db.session.commit()
+            new_count = Collection.query.filter_by(upload_id=upload_id).count()
+            return jsonify({'success': True, 'newCount': new_count, 'liked': False})
+        else:
+            new_collection = Collection(
+                upload_id=upload_id, user_id=current_user.id)
+            db.session.add(new_collection)
+            db.session.commit()
+            new_count = Collection.query.filter_by(upload_id=upload_id).count()
+            return jsonify({'success': True, 'newCount': new_count, 'liked': True})
+    except Exception as e:
+        print(f"collectError: {e}")
+        return jsonify({'success': False, 'message': str(e)}), 500
 
 
 @main_bp.route('/post_comment/<int:upload_id>', methods=['POST'])
@@ -168,7 +172,9 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('main.login'))
     return render_template('register.html', title='Register', form=form)
-@main_bp.route('/search',methods=['GET','POST'])
+
+
+@main_bp.route('/search', methods=['GET', 'POST'])
 @login_required
 def search():
     form = g.search_form
@@ -186,5 +192,13 @@ def search():
         # Fetch the data for each upload
         grouped_details = fetch_data_gallery()
         # Filter the grouped details based on the search query
-        filtered_details = {upload_id: details for upload_id, details in grouped_details.items() if upload_id in [upload.id for upload in uploads]}
-    return render_template('search.html', title='search', form=form, grouped_details=filtered_details)
+        filtered_details = {upload_id: details for upload_id, details in grouped_details.items(
+        ) if upload_id in [upload.id for upload in uploads]}
+
+        comments_with_user = db.session.query(
+            Upload.id,
+            Comment.comment_content,
+            Comment.comment_time,
+            User.username
+        ).select_from(Comment).join(User).outerjoin(Upload, Upload.id == Comment.upload_id).all()
+    return render_template('search.html', title='search', form=form, grouped_details=filtered_details, comments_with_user=comments_with_user)
